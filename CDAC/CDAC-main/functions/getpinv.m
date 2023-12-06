2fc9f3366cc381b61bca5fa1a408988b64a75ed5
Rachele Nebbia <75611841+rachele182@users.noreply.github.com>
2023-12-06 15:25:59 +0100

    Delete CDAC directory

--- CDAC/CDAC-main/functions/getpinv.m
+++
@@ -1,7 +0,0 @@
-%% Robust pseudo Inverse
-%% Compute pseudo-inverse of rectangular matrix robust to singularities (Levenberg-Marquardt algoritm) 
-
-function J_inv = getpinv(J,mu)
-s = size(J,1);
-J_inv = J'*pinv(J*J' + mu*eye(s) );
-end
